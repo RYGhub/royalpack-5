e92fbde772ae8b95081010c532cea8c0e8b9610f
Alby1 <30355916+Alby1@users.noreply.github.com>
2019-12-05 13:39:09 +0100

    Eat alby

--- royalpack/commands/eat.py
+++ royalpack/commands/eat.py
@@ -37,7 +37,10 @@ class EatCommand(Command):
         "chiara": "🚺 Hai mangiato {food}. \n[i]Sa un po' di biscotto, ma per lo più sa di curcuma, pepe e spezie varie.[/i]",
         "marco": "🚹 Hai mangiato {food}. \n[i]Sa di carlino <.<[/i]",
         "steffo": "🚹 Hai mangiato {food}. \n[i]Sa di gelato e di Coca Cola.[/i]",
-        
+        "alby": "🚹 Hai mangiato {food}. \n[i]Sa di judo e golf.[/i]",
+        "malbyx": "🚹 Hai mangiato {food}. \n[i]Sa di judo e golf.[/i]",
+
+
         #altro
         "tonnuooooooro": "👻 Il {food} che hai mangiato era posseduto.\n[i]Spooky![/i]",
         "uranio": "☢️ L'{food} che hai mangiato era radioattivo.\n[i]Stai brillando di verde![/i]",
