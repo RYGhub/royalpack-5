f2dde503304cd5519640d17696e521940b8b2280
Stefano Pigozzi <ste.pigozzi@gmail.com>
2020-05-28 17:02:24 +0200

    Update files

--- publish.bat
+++ publish.bat
@@ -1,2 +1,2 @@
 git commit -am "publish: %1"
-git push && poetry build && poetry publish && hub release create "%1" -m "Royalnet %1" && sentry-cli releases deploys "%1" new --env production -n "GitHub %1"
+git push && poetry build && poetry publish && hub release create "%1" -m "Royalnet %1"
