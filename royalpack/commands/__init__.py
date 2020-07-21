b7f099d499c1a11ff9782feb30726978199c6e0f
Stefano Pigozzi <ste.pigozzi@gmail.com>
2020-07-21 22:43:20 +0200

    Rimuovi tutti i comandi relativi alla chat vocale

--- royalpack/commands/__init__.py
+++ royalpack/commands/__init__.py
@@ -11,58 +11,32 @@ from .diarioquote import DiarioquoteCommand
 from .diarioshuffle import DiarioshuffleCommand
 from .dota import DotaCommand
 from .eat import EatCommand
-from .elevatormusic import ElevatormusicCommand
 from .emojify import EmojifyCommand
 from .eval import EvalCommand
 from .exec import ExecCommand
 from .fortune import FortuneCommand
-from .funkwhale import FunkwhaleCommand
-from .funkwhalealbum import FunkwhalealbumCommand
-from .funkwhaleplaylist import FunkwhaleplaylistCommand
 from .givefiorygi import GivefiorygiCommand
 from .givetreasure import GivetreasureCommand
-from .googlevideo import GooglevideoCommand
 from .help import HelpCommand
-from .lazyfunkwhale import LazyfunkwhaleCommand
-from .lazyfunkwhalealbum import LazyfunkwhalealbumCommand
-from .lazyfunkwhaleplaylist import LazyfunkwhaleplaylistCommand
-from .lazygooglevideo import LazygooglevideoCommand
-from .lazypeertube import LazypeertubeCommand
-from .lazyplay import LazyplayCommand
-from .lazysoundcloud import LazysoundcloudCommand
-from .lazyyahoovideo import LazyyahoovideoCommand
-from .lazyyoutube import LazyyoutubeCommand
 from .leagueoflegends import LeagueoflegendsCommand
 from .magickfiorygi import MagickfiorygiCommand
 from .magicktreasure import MagicktreasureCommand
 from .matchmaking import MatchmakingCommand
-from .pause import PauseCommand
-from .peertube import PeertubeCommand
 from .peertubeupdates import PeertubeUpdatesCommand
 from .ping import PingCommand
-from .play import PlayCommand
-from .playmode import PlaymodeCommand
 from .pmots import PmotsCommand
 from .dog import DogCommand
-from .queue import QueueCommand
 from .rage import RageCommand
 from .reminder import ReminderCommand
 from .royalpackversion import RoyalpackCommand
 from .ship import ShipCommand
-from .skip import SkipCommand
 from .smecds import SmecdsCommand
-from .soundcloud import SoundcloudCommand
 from .spell import SpellCommand
 from .steammatch import SteammatchCommand
 from .steampowered import SteampoweredCommand
-from .summon import SummonCommand
 from .treasure import TreasureCommand
 from .trivia import TriviaCommand
 from .userinfo import UserinfoCommand
-from .videochannel import VideochannelCommand
-from .voicestatus import VoicestatusCommand
-from .yahoovideo import YahoovideoCommand
-from .youtube import YoutubeCommand
 
 # Enter the commands of your Pack here!
 available_commands = [
@@ -78,58 +52,32 @@ available_commands = [
     DiarioshuffleCommand,
     DotaCommand,
     EatCommand,
-    ElevatormusicCommand,
     EmojifyCommand,
     EvalCommand,
     ExecCommand,
     FortuneCommand,
-    FunkwhalealbumCommand,
-    FunkwhaleCommand,
-    FunkwhaleplaylistCommand,
     GivefiorygiCommand,
     GivetreasureCommand,
-    GooglevideoCommand,
     HelpCommand,
-    LazyfunkwhalealbumCommand,
-    LazyfunkwhaleCommand,
-    LazyfunkwhaleplaylistCommand,
-    LazygooglevideoCommand,
-    LazypeertubeCommand,
-    LazyplayCommand,
-    LazysoundcloudCommand,
-    LazyyahoovideoCommand,
-    LazyyoutubeCommand,
     LeagueoflegendsCommand,
     MagickfiorygiCommand,
     MagicktreasureCommand,
     MatchmakingCommand,
-    PauseCommand,
-    PeertubeCommand,
     PeertubeUpdatesCommand,
     PingCommand,
-    PlayCommand,
-    PlaymodeCommand,
     PmotsCommand,
     DogCommand,
-    QueueCommand,
     RageCommand,
     ReminderCommand,
     RoyalpackCommand,
     ShipCommand,
-    SkipCommand,
     SmecdsCommand,
-    SoundcloudCommand,
     SpellCommand,
     SteammatchCommand,
     SteampoweredCommand,
-    SummonCommand,
     TreasureCommand,
     TriviaCommand,
     UserinfoCommand,
-    VideochannelCommand,
-    VoicestatusCommand,
-    YahoovideoCommand,
-    YoutubeCommand,
 ]
 
 # Don't change this, it should automatically generate __all__
