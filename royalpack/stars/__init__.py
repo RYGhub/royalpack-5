8d6c57ad309d350eff67027bafe576294e834508
Stefano Pigozzi <ste.pigozzi@gmail.com>
2020-08-29 23:59:45 +0200

    Add new diario method

--- royalpack/stars/__init__.py
+++ royalpack/stars/__init__.py
@@ -13,6 +13,7 @@ from .api_user_ryg import ApiUserRygStar
 from .api_user_ryg_list import ApiUserRygListStar
 from .api_user_avatar import ApiUserAvatarStar
 from .api_auth_login_osu import ApiAuthLoginOsuStar
+from .api_diario_range import ApiDiarioRangeStar
 
 # Enter the PageStars of your Pack here!
 available_page_stars = [
@@ -30,6 +31,7 @@ available_page_stars = [
     ApiUserRygListStar,
     ApiUserAvatarStar,
     ApiAuthLoginOsuStar,
+    ApiDiarioRangeStar,
 ]
 
 # Don't change this, it should automatically generate __all__
